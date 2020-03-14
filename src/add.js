c0df0347327deec76f25454a87b28b350a337814
Renat <suleymanov_renat@mail.ru>
2020-03-15 00:07:54 +0300

    правки

--- src/add.js
+++ src/add.js
@@ -6,10 +6,12 @@ function Character(name, type) {
   this.health = 100;
   this.attack = 10;
   this.defence = 40;
+}
 
-  this.damage = (points) => {
-    if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
-    }
+Character.prototype = {
+  damage: points => {
+  if (this.health >= 0) {
+    this.health -= points * (1 - this.defence / 100);
   }
+}
 }
